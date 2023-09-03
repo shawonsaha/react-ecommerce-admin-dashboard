403bef5e0d4be65f2a751aa372e770d77a54b76a
shawonsaha <shawondroid@gmail.com>
2023-09-03 14:19:29 +0600

    Revenue section

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import { BsCurrencyDollar } from "react-icons/bs";
 import { GoPrimitiveDot } from "react-icons/go";
 
+import { GoDotFill } from "react-icons/go";
+
 import { Stacked, Pie, Button, SparkLine } from "../components";
 import {
   earningData,
@@ -57,6 +59,63 @@ const Ecommerce = () => {
               <p className="text-sm text-gray-400 mt-1">{item.title}</p>
             </div>
           ))}
+        </div>
+      </div>
+      <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780">
+        <div className="flex justify-between">
+          <p className="font-semibold text-xl">Revenue Update</p>
+          <div className="flex items-center gap-4">
+            <p className="flex items-center gap-2 text-green-600 hover:drop-shadow-xl">
+              <span>
+                <GoDotFill />
+              </span>
+              <span>Budget</span>
+            </p>
+          </div>
+        </div>
+
+        <div className="mt-10 flex gap-10 flex-wrap justify-center">
+          <div className="border-r-1 border-color m-4 pr-10">
+            <div>
+              <p>
+                <span className="text-3xl font-semibold">$94,776</span>
+                <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">
+                  21%
+                </span>
+              </p>
+              <p className="text-gray-500 text-xs">Budget</p>
+            </div>
+
+            <div className="mt-8">
+              <p>
+                <span className="text-3xl font-semibold">$21,469</span>
+              </p>
+              <p className="text-gray-500 text-xs">Expense</p>
+            </div>
+
+            <div className="mt-5">
+              <SparkLine
+                currentColor="blue"
+                id="line-sparkline"
+                type="Line"
+                height="80px"
+                width="250px"
+                data={SparklineAreaData}
+                color="blue"
+              />
+            </div>
+            <div>
+              <Button
+                color="white"
+                bgColor="blue"
+                text="Download Report"
+                borderRadius="10px"
+              />
+            </div>
+          </div>
+          <div>
+            <Stacked width="320px" height="360px" />
+          </div>
         </div>
       </div>
     </div>
